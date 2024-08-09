bf1e2a106fd2349c24235ce9591a560ba47aba3c
XXXAADI <asingh12131415161718@gmail.com>
2024-08-09 17:02:39 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI-KATAAR.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "Kattar",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "PREM BABU", 
+	description: "Just Respond",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("JIDDI") ||
+     react.includes("jiddi") || react.includes("@Jiddi girl 🥵") || react.includes("@Kattar Thakurain") ||
+react.includes("kattar") ||
+react.includes("KATTAR")) {
+		var msg = {
+				body: `Bete yha se marna chalu karunga aur india border tak gand me chappal marte le jaunga samjha😹`,
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😇", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
