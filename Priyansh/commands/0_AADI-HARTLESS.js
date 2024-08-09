244876776799b82eb1aa86cd3ac76f8842d8bb78
XXXAADI <asingh12131415161718@gmail.com>
2024-08-09 23:44:54 +0530

    Update 0_AADI-HARTLESS.js

--- Priyansh/commands/0_AADI-HARTLESS.js
+++ Priyansh/commands/0_AADI-HARTLESS.js
@@ -19,7 +19,8 @@ react.includes("@Masoom girl 🥀🦋💖") ||
 react.includes("Masoom girl") ||     
 react.includes("masoom")) {
     var msg = {
-        body: `👉 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐐𝐔𝐄𝐄𝐍 👈`,attachment: fs.createReadStream(__dirname + `/noprefix/babua.jpeg`)
+        body: `🥰 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐐𝐔𝐄𝐄𝐍 🥰
+    𝐃𝐔𝐑 𝐇𝐀𝐓 𝐓𝐄𝐑𝐄𝐊𝐎 𝐊𝐎𝐈 𝐀𝐔𝐑 𝐊𝐀𝐌 𝐍𝐀𝐇𝐈 𝐉𝐀𝐁 𝐃𝐀𝐊𝐇𝐎 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐇𝐀𝐑𝐓𝐋𝐄𝐒𝐒 𝐊𝐀𝐑𝐓𝐄 𝐑𝐄𝐇𝐓𝐄 𝐇𝐎 😝😝`,attachment: fs.createReadStream(__dirname + `/noprefix/babua.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
