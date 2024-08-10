9f788f50973081f44de5144e9d93ef9c08066407
XXXAADI <asingh12131415161718@gmail.com>
2024-08-11 00:21:02 +0530

    Update 0_AADI-DP-1.js

--- Priyansh/commands/0_AADI-DP-1.js
+++ Priyansh/commands/0_AADI-DP-1.js
@@ -91,7 +91,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
           ctx.beginPath();
  
  
-  ctx.drawImage(baseAvt1, 20, 90, 500, 300);
+  ctx.drawImage(baseAvt1, 20, 90, 300, 500);
  
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
