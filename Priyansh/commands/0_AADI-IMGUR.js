7e4c2f4356c031f32cac65777e89e1ab719115d3
XXXAADI <asingh12131415161718@gmail.com>
2024-08-11 08:09:50 +0530

    Update 0_AADI-IMGUR.js

--- Priyansh/commands/0_AADI-IMGUR.js
+++ Priyansh/commands/0_AADI-IMGUR.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const { downloadFile } = require("../../utils/index.js");
 
 module.exports.config = {
-  name: "imgur",
+  name: "IMGUR",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
