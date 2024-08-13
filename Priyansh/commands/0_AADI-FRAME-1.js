049388e0e6ba30a7094166fe006e3a3672fd4fab
XXXAADI <asingh12131415161718@gmail.com>
2024-08-13 10:37:36 +0530

    Update 0_AADI-FRAME-1.js

--- Priyansh/commands/0_AADI-FRAME-1.js
+++ Priyansh/commands/0_AADI-FRAME-1.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(230, 230), 540, 120).composite(circleTwo.resize(250, 250), 65, 65);
+    batgiam_img.composite(circleOne.resize(230, 230), 540, 120).composite(circleTwo.resize(230, 260), 70, 75);
     
     let raw = await batgiam_img.getBufferAsync("image/jpeg");
     
