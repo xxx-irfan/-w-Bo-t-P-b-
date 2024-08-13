256267e803aac10d04f58b2ffe142d739f72051b
XXXAADI <asingh12131415161718@gmail.com>
2024-08-13 09:27:27 +0530

    Update 0_AADI-DP-4.js

--- Priyansh/commands/0_AADI-DP-4.js
+++ Priyansh/commands/0_AADI-DP-4.js
@@ -56,7 +56,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
  
   var background = [
  
-    "https://i.imgur.com/zEqzTyl.jpeg"
+    "https://i.imgur.com/tXgEbA8.jpeg"
 ];
   var rd = background[Math.floor(Math.random() * background.length)];
  
