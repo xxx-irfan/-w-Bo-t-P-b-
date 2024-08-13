8b22f252b196f5c27035b41ee7750f4ef6e6d58c
XXXAADI <asingh12131415161718@gmail.com>
2024-08-13 13:56:56 +0530

    Update 0_AADI_KISS-YOU.js

--- Priyansh/commands/0_AADI_KISS-YOU.js
+++ Priyansh/commands/0_AADI_KISS-YOU.js
@@ -21,7 +21,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("kiss me") ||
 react.includes("KISS YOU")) {
 		var msg = {
-				body: `𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`,attachment: fs.createReadStream(__dirname + `/AADI/KK10.png`)
+				body: `𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞`,attachment: fs.createReadStream(__dirname + `/AADI/KK10.DP`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💋", event.messageID, (err) => {}, true)
