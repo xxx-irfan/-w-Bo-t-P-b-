1986e4c4ddb3528a73d18bf5d5cb2f590b21008c
XXXAADI <asingh12131415161718@gmail.com>
2024-08-15 17:07:30 +0530

    Update 0_dp_name.js

--- Priyansh/commands/0_dp_name.js
+++ Priyansh/commands/0_dp_name.js
@@ -48,7 +48,7 @@ module.exports.run = async function ({ api, event, args, Users }) {
   let pathImg = __dirname + `/cache/drake.png`;
   const text = args.join(" ").trim().replace(/\s+/g, " ").replace(/(\s+\=)/g, "+").replace(/\|\s+/g, "+").split("+");
   let getImage = (
-    await axios.get(encodeURI(`https://i.imgur.com/Vu0AYmH.jpg`), {
+    await axios.get(encodeURI(`https://i.imgur.com/XQg9kQR.jpeg`), {
       responseType: "arraybuffer",
     })
   ).data;
