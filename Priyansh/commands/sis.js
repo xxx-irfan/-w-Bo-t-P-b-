6872fd74415326e38524f3046c24c702a0264189
XXXAADI <asingh12131415161718@gmail.com>
2024-08-16 23:43:32 +0530

    Update sis.js

--- Priyansh/commands/sis.js
+++ Priyansh/commands/sis.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'sis.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/n2FGJFe.jpg", path); 
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/3vfcL97.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(191, 191), 93, 111).composite(circleTwo.resize(190, 190), 434, 107);
+    batgiam_img.composite(circleOne.resize(200, 200), 77, 152).composite(circleTwo.resize(200, 200), 440, 155);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
