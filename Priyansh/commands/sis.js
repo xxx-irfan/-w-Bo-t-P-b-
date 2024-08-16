9629e5f6a8783e40c30655966683ccc0df0d05f9
XXXAADI <asingh12131415161718@gmail.com>
2024-08-16 23:46:13 +0530

    Update sis.js

--- Priyansh/commands/sis.js
+++ Priyansh/commands/sis.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'sis.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/3vfcL97.jpeg", path); 
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/vW6rZq0.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
