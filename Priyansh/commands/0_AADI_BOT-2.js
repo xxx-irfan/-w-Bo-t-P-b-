1183dd417c69c79d9ad62f8962e09d35ce6c3b86
XXXAADI <asingh12131415161718@gmail.com>
2024-09-01 07:17:27 +0530

    Update 0_AADI_BOT-2.js

--- Priyansh/commands/0_AADI_BOT-2.js
+++ Priyansh/commands/0_AADI_BOT-2.js
@@ -68,7 +68,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
   
   if (event.body.indexOf("bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `『٭』${name}『٭』\n\n
+      body: `💙───${name}───💙\n\n
      
       ${rand}   
         \n\n                       `
