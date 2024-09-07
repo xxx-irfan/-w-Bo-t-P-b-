66ad88f57483cb6f77c070c065a51cb0b733d899
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-07 21:38:12 +0530

    Update music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -20,30 +20,20 @@ module.exports = {
   },
 
   run: async function ({ api, event, args }) {
-    // let songName, type;
+    let songName, type;
 
-    // if (
-    //   args.length > 1 &&
-    //   (args[args.length - 1] === "audio" || args[args.length - 1] === "video")
-    // ) {
-    //   type = args.pop();
-    //   songName = args.join(" ");
-    // } else {
-    //   songName = args.join(" ");
-    //   type = "audio";
-    // }
-
-    if (args.length === 0) {
-      return api.sendMessage(
-        "Please provide the name of the song to download.",
-        threadID,
-        messageID
-      );
+    if (
+      args.length > 1 &&
+      (args[args.length - 1] === "audio" || args[args.length - 1] === "video")
+    ) {
+      type = args.pop();
+      songName = args.join(" ");
+    } else {
+      songName = args.join(" ");
+      type = "audio";
     }
 
-    const songName = args.join(" ");
-
-    const apiUrl = `https://known-billy-priyanshbot-3a9e048f.koyeb.app/yt?song=${encodeURIComponent(songName)}`;
+    const apiUrl = `https://07e8363c-50e9-433d-a6b5-c9e18ca3e2df-00-3m6psysyh8j6u.sisko.replit.dev/yts?song=${encodeURIComponent(songName)}&type=${encodeURIComponent(type)}&apikey=priyansh-here`;
 
     const processingMessage = await api.sendMessage(
       "✅ Processing your request. Please wait...",
@@ -79,7 +69,7 @@ module.exports = {
       await api.sendMessage(
         {
           attachment: fs.createReadStream(downloadPath),
-          body: `🖤 Title: ${filename}\n\n Here is your song 🎧:`,
+          body: `Here's the song: ${filename}`,
         },
         event.threadID,
         () => {
