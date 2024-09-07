4cd1e7d66620c3f0d938b228f316f59f6f370d5d
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-07 15:26:48 +0530

    Update 0_AADI-FRIEND.js

--- Priyansh/commands/0_AADI-FRIEND.js
+++ Priyansh/commands/0_AADI-FRIEND.js
@@ -20,7 +20,7 @@ module.exports.onLoad = async() => {
   const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
   const { downloadFile } = global.utils;
   const dirMaterial = __dirname + `/cache/canvas/`;
-  const path = resolve(__dirname, 'cache/canvas', 'Bbro.png');
+  const path = resolve(__dirname, 'cache/canvas', 'Bbro.png1');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
   if (!existsSync(path)) await downloadFile("https://i.imgur.com/BfY2u0o.jpg", path); 
 }
