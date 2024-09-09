1138697ca54212fe0bf3358ff55368ff9e7402c7
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-09 07:46:05 +0530

    Update love.js

--- Priyansh/commands/love.js
+++ Priyansh/commands/love.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'ay.jpeg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/yynJVyB.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/jeabWYT.jpeg", path);
 }
  
 async function makeImage({ one, two }) {
