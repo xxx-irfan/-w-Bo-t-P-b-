1b8b19bbc211b88e445f501634df538d176f206e
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-09 19:54:46 +0530

    Update song.js

--- Priyansh/commands/song.js
+++ Priyansh/commands/song.js
@@ -5,7 +5,7 @@ const contentDisposition = require("content-disposition");
 
 module.exports = {
   config: {
-    name: "song",
+    name: "Song",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
