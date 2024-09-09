02c660aa429c92d24c8e36f1b4474661784ad36c
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-09 07:49:31 +0530

    Update love.js

--- Priyansh/commands/love.js
+++ Priyansh/commands/love.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
  
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(170, 170), 100, 120).composite(circleTwo.resize(170, 170), 445, 120);
+    batgiam_img.composite(circleOne.resize(180, 200), 100, 200).composite(circleTwo.resize(160, 200), 495, 120);
  
     let raw = await batgiam_img.getBufferAsync("image/jpeg");
  
