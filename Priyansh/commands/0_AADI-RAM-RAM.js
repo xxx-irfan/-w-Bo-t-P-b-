08f2669537e2c5d9b6f77ed2cba719021b983561
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-14 11:15:02 +0530

    Update 0_AADI-RAM-RAM.js

--- Priyansh/commands/0_AADI-RAM-RAM.js
+++ Priyansh/commands/0_AADI-RAM-RAM.js
@@ -1,30 +1,24 @@
-//@Prem-babu3608
-////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
 const fs = require("fs");
 module.exports.config = {
         name: "Ram",
-    version: "1.1.1",
+    version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
-        description: "THIS BOT IS PREM SHARMA",
+        credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+        description: "hihihihi",
         commandCategory: "no prefix",
+        usages: "sub",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
-        let react = event.body.toLowerCase();
-        if(react.includes("jay shree ram") ||
-     react.includes("Jay shree ram") || react.includes("JAY SHREE RAM") || react.includes("jai shree ram") ||
-react.includes("Jai shree ram") ||
-react.includes("JAI SHREE RAM")) {
+        if (event.body.indexOf("JAY SHREE RAM")==0 || event.body.indexOf("Jay shree ram")==0 || event.body.indexOf("jay shree ram")==0 || event.body.indexOf("Jay Shri Ram")==0) {
                 var msg = {
-                                body: `💙 ─── जय श्री राम  ─── 💙
- 💙  ─── जय श्री राम  ─── 💙`,
+                                body: "__🥰  𝐌𝐀𝐈𝐍 𝐓𝐎 𝐒𝐇𝐑𝐄𝐄 𝐑𝐀𝐌 𝐉𝐈  𝐊𝐀 𝐃𝐄𝐄𝐖𝐀𝐍𝐀 𝐇𝐔 𝐏𝐘𝐀𝐑 𝐒𝐄 𝐁𝐎𝐋𝐎 𝐉𝐀𝐘 𝐒𝐇𝐑𝐄𝐄 𝐑𝐀𝐌 😍 ❤️",
+                                attachment: fs.createReadStream(__dirname + `/AADI/#ram_#ramlalaa_#ayodhya_#siyaram_in_2024_|_Ram_photos,_Shree_ram_photos,_Shri_ram_photo(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙏", event.messageID, (err) => {}, true)
                 }
         }
         module.exports.run = function({ api, event, client, __GLOBAL }) {
